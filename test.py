8524bf585a5ed6b33bdc948e600d0451cd178d27
wertrain <wertrain@gmail.com>
2016-01-31 14:18:45 +0900

    WIP

--- test.py
+++ test.py
@@ -2,9 +2,23 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/lib')
 
+import re
 import urllib2
 from bs4 import BeautifulSoup
 from my.util import i2ch
 
 if __name__ == '__main__':
-    print 'スクリプトの時だけ  __name__ = %s' % __name__
+    f = open('test.html')
+    html = f.read().decode('shift_jis', 'ignore')
+    f.close()
+    
+    p = re.compile(u'([a-zA-Z]{1})([a-zA-Z0-9\_-]){3,15}', re.IGNORECASE)
+    bs = BeautifulSoup(html)
+    for dt in bs.find_all('dt'):
+        #print unicode(str(dt), 'utf-8', 'ignore')
+        dd = dt.findNextSibling('dd')
+        #print unicode(str(dd), 'utf-8', 'ignore')
+        #match = p.search('([a-zA-Z]{1})([a-zA-Z0-9\_-]){3,15}', dd.text)
+        iterator = p.finditer(dd.text)
+        for match in iterator:
+            print match.group()   # 1回目: ca      2回目: ca   
